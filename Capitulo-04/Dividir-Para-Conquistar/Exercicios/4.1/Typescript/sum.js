1789c23d069c44fde973e3aaa2e49206df1bc0a4
Rafael Toledo <toledorafael304@gmail.com>
2025-05-22 07:58:25 -0300

    Add TypeScript implementation of sum function with example usage

---
+++ Capitulo-04/Dividir-Para-Conquistar/Exercicios/4.1/Typescript/sum.js
@@ -0,0 +1,11 @@
+function sum(list) {
+    if (list.length === 0) {
+        return 0;
+    }
+    else {
+        return list[0] + sum(list.slice(1));
+    }
+}
+console.log(sum([1, 2, 3, 4]));
+console.log(sum([2, 5, 6, 7]));
+console.log(sum([3, 5, 7, 9]));
